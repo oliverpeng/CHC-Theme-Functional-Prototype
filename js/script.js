81a53780dae75ff77eb4eea1ff00e2794ec0a2b8
Oliver Peng <oliverpeng@gmail.com>
2011-10-26 11:54:08 -0500

    javascript resizing of page

--- js/script.js
+++ js/script.js
@@ -17,7 +17,30 @@ $(document).ready( function() {
 });
 
 
-
+$(document).ready( function() {
+    var defaultHeight = $('#container').height();
+    var defaultWidth = $('#container').width();
+    var $window = $(window);
+    
+    $window.resize( function(e) {
+        var newHeight = $window.height();
+        if(newHeight <= defaultHeight) {
+            // use defaults
+        } else {
+            var scale = newHeight / defaultHeight;
+            var newWidth = Math.floor(scale*defaultWidth);
+            console.log(newWidth);
+            $('#container').width( newWidth );
+            $('#container').height( newHeight );
+            
+            var $main = $('#main');
+            var hPadding = $main.innerWidth() - $main.width();
+            var vPadding = $main.innerHeight() - $main.height();
+            $main.width( newWidth - hPadding );
+            $main.height( newHeight - $('header').innerHeight() - $('nav').innerHeight() - $('footer').innerHeight() - vPadding - 1);
+        }
+    }).resize();
+});
 
 
 
