9bb6dfe0723fc18b31a3d4240209db7de6234728
Oliver Peng <oliverpeng@gmail.com>
2011-10-30 01:29:57 -0500

    Flickr loading complete
    Fleshed out functions to pull info from Flickr photostream

--- js/media.js
+++ js/media.js
@@ -1,6 +1,4 @@
-$(document).ready( function() {
-    
-    
+$(document).ready( function() {   
     /* Simple helper function to convert key, value pairs in object to URL query string
        Sample input:
        {
@@ -24,15 +22,57 @@ $(document).ready( function() {
         return array.join('&');
     }
     
-    /* Flickr API: http://www.flickr.com/services/api/
+    /* Builds Flickr photo URL based on http://www.flickr.com/services/api/misc.urls.html
+       size parameter can be string or array of strings. If an array is provided, an object is returned
+       keyed by size.
+       
+       Example:
+       Input - ['m','s']
+       Output - { m: 'url1', s: 'url2' }
     */
-    function fetchPhotos(options) {
+    function buildFlickrPhotoURL(params) {
+        params = params || {};
+        var defaultParams = {
+            farmId: 1, // override
+            serverId: 6167, // override
+            id: 1, // override
+            secret: '80bc2d6509', // override
+            size: 's' // m,s,t,z,b or array of sizes ie ['s', 'm']
+        }
+        
+        var p = $.extend(defaultParams, params);
+        
+        console.log(p);
+        
+        // http://farm{farm-id}.static.flickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
+        var base_url = 'http://farm'+ p.farmId +'.static.flickr.com/'+ p.serverId +'/'+ p.id +'_'+ p.secret;
+        if (typeof(p.size) === 'string') {
+            return base_url +'_'+ p.size +'.jpg';
+        } else if ( p.size instanceof Array ) {
+            var returnObj = {};
+            $.each(p.size, function(i, v) {
+                returnObj[v] = base_url + '_' + v + '.jpg';
+            });
+            return returnObj;
+        } else {
+            return ''; // silent fail
+        }
+    }
+    
+    /* Make ajax request to fetch CHC's Flickr photostream
+       Options:
+        page     - page #
+        per_page - how many images per request
+        callback - callback function to run upon successful ajax request
+      
+       Based on Flickr API: http://www.flickr.com/services/api/
+    */
+    function ajaxFetchPhotos(options) {
         options = options || {};
         var defaultOpts = {
             'page': 1,
             'per_page': 30,
-            'callback': function(data, textStatus, jqXHR) { /* do nothing */ },
-            'format': 'json'
+            'callback': function(json, textStatus, jqXHR) { /* do nothing */ }
         }
         options = $.extend({}, defaultOpts, options);
         
@@ -40,6 +80,7 @@ $(document).ready( function() {
             'method': 'flickr.people.getPublicPhotos',
             'api_key': '0dfd190065b8726df9f0670b7ec3dd0e',
             'user_id': '46965176@N02',
+            'format': 'json',
             'nojsoncallback': 1
         }
         $.extend(fetchParams, { page: options.page, per_page: options.per_page, format: options.format });
@@ -50,13 +91,17 @@ $(document).ready( function() {
             success: options.callback
         });
     }
-    fetchPhotos({
-        page: 2,
-        callback: function(data) {
-            console.log('YEEHAW', data);
+    
+    var $ul = $('#photostream ul');
+    ajaxFetchPhotos({
+        page: 1,
+        callback: function(json) {
+            $.each(json.photos.photo, function(i, photo) {
+                var thumbUrl = buildFlickrPhotoURL({farmId: photo.farm, serverId: photo.server, id: photo.id, secret: photo.secret, size: 's' });
+                var $image = $( document.createElement('img') ).attr('src', thumbUrl);
+                $( document.createElement('li') ).append( $image ).appendTo($ul);
+            });
         }
-    });
-    
-    
+    });    
     
 });
