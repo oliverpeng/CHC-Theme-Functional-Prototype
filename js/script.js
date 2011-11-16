c7ebd070faa43022c42b5ecbd996b85520b103f8
Oliver Peng <oliverpeng@gmail.com>
2011-11-16 03:45:10 -0600

    Kill page resize script

--- js/script.js
+++ js/script.js
@@ -37,37 +37,3 @@ $(document).ready( function() {
         $contact.dialog('open');
     });
 });
-
-/* Dynamically resize page depending on viewport */
-$(document).ready( function() {
-    var $window = $(window);
-    var $container = $('#container');
-    var $main = $('#main');
-    
-    if($container.hasClass('no-resize')) {
-        return;
-    }
-    
-    var defaultHeight = $container.height();
-    var defaultWidth = $container.width();
-    
-    var hPadding = $main.innerWidth() - $main.width();
-    var vPadding = $main.innerHeight() - $main.height();
-    var innerHeights = $('header').innerHeight() + $('nav').innerHeight() + $('footer').innerHeight();
-    
-    $window.resize( function(e) {
-        var newHeight = $window.height();
-        if(newHeight <= defaultHeight) {
-            // use defaults
-        } else {
-            var scale = newHeight / defaultHeight;
-            var newWidth = Math.floor(scale*defaultWidth);
-            
-            $container.width( newWidth );
-            $container.height( newHeight );
-            
-            $main.width( newWidth - hPadding );
-            $main.height( newHeight - innerHeights - vPadding );
-        }
-    }).resize();
-});
